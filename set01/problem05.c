2b5d2c5d85d12ce60fd22016608a896d3a6050b2
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-10-12 16:19:14 +0530

    committed 10/12/2023, 4:19:14 PM

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
 
+int input();
+int compare(int a, int b, int c);
+void output(int a, int b, int c, int largest);
+
+int main()
+{
+    int a,b,c,lrg=0;
+    a=input();
+    b=input();
+    c=input();
+    lrg=compare(a,b,c);
+    output(a,b,c,lrg);
+    return 0;
+}
+
+int input()
+{
+    int x;
+    printf("Enter number:");
+    scanf("%d",&x);
+    return x;
+}
+
+int compare(int a,int b,int c)
+{
+    int lrg=0;
+    if(a>=b && a>=c)
+    {
+        lrg=a;
+    }
+    else if(b>=a && b>=c)
+    {
+        lrg=b;
+    }
+    else
+    {
+        lrg=c;
+    }
+    return lrg;
+}
+
+void output(int a, int b, int c, int largest)
+{
+    printf("The largest of %d,%d and %d is %d",a,b,c,largest);
+}
