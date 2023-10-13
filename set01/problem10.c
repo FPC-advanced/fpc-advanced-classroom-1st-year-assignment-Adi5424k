4a8af8f7a3a885971722efb56397ac98e3e85d86
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-10-13 10:36:20 +0530

    committed 10/13/2023, 10:36:20 AM

---
+++ set01/problem10.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+void input_two_strings(char *string1, char *string2);
+int stringcompare(char *string1, char *string2);
+void output(char *string1, char *string2, int result);
+
+int main()
+{
+    int r;
+    char str1[100],str2[100];
+    input_two_strings(str1,str2);
+    r=stringcompare(str1,str2);
+    output(str1,str2,r);
+    return 0;
+}
+
+void input_two_strings(char *string1, char *string2)
+{
+    printf("Enter string one: ");
+    scanf("%s",string1);
+    printf("Enter string two: ");
+    scanf("%s",string2);
+}
+
+int stringcompare(char *string1, char *string2)
+{
+    int i;
+    for(i=0;string1[i]==string2[i] && string1[i]!='\0' && string2[i]!='\0';i++);
+    if(string1[i]>string2[i])
+    {
+        return 1;
+    }
+    else if(string1[i]<string2[i])
+    {
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+void output(char *string1, char *string2, int result)
+{
+    if(result==1)
+    {
+        printf("%s is greater than %s",string1,string2);
+    }
+    else if(result==0)
+    {
+        printf("%s is greater than %s",string2,string1);
+    }
+    else
+    {
+        printf("Strings are equal");
+    }
+}
