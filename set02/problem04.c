8f90435369e9129183a7799e7fe072522c1596d6
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-10-16 10:45:47 +0530

    committed 10/16/2023, 10:45:47 AM

---
+++ set02/problem04.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int input_array_size();
+void input_array(int n, int a[n]);
+int sum_composite_numbers(int n, int a[n]);
+void output(int sum);
+
+int main()
+{
+    int n,sum=0;
+    n=input_array_size();
+    int a[n];
+    input_array(n,a);
+    sum=sum_composite_numbers(n,a);
+    output(sum);
+    return 0;
+}
+
+int input_array_size()
+{
+    int x;
+    printf("Enter array size: ");
+    scanf("%d",&x);
+    return x;
+}
+
+void input_array(int n, int a[n])
+{
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+}
+
+int sum_composite_numbers(int n, int a[n])
+{
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>3)
+        {
+            for(int j=2;j<a[i];j++)
+            {
+                if(a[i]%j==0)
+                {
+                    sum+=a[i];
+                    break;
+                }
+            }
+        }
+    }
+    return sum;    
+}
+
+void output(int sum)
+{
+    printf("%d",sum);
+}
