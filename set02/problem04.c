1064bae84d9f4e78f427a45d8659270fc401179f
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-10-30 10:08:59 +0530

    committed 10/30/2023, 10:08:58 AM

--- set02/problem04.c
+++ set02/problem04.c
@@ -32,6 +32,25 @@ void input_array(int n, int a[n])
     }
 }
 
+int is_composite(int n)
+{
+    if(n<=1)
+    {
+        return 0;
+    }
+    else
+    {
+        for(int i=2;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                return 1;
+            }
+        }
+        return 0;   
+    }
+}
+
 int sum_composite_numbers(int n, int a[n])
 {
     int sum=0;
