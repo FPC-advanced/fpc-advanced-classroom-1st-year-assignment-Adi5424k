e87a72b7abaf0132e65845249df6bd272a0d7a17
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-10-31 11:09:44 +0530

    committed 10/31/2023, 11:09:43 AM

--- set03/problem01.c
+++ set03/problem01.c
@@ -11,6 +11,9 @@ int main()
 
 void input(float *x1, float *y1, float *x2, float *y2)
 {
-    scanf("%f%f",&x1,&y1);
-    scanf("%f%f",&x1,&y1);
+    scanf("%f%f",x1,y1);
+    scanf("%f%f",x2,y2);
 }
+
+float find_distance(float x1, float y1, float x2, float y2)
+{}
