180bdc32d942e8b0fe7a35838513d1dd6d31e838
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-10-31 11:12:14 +0530

    committed 10/31/2023, 11:12:13 AM

--- set03/problem01.c
+++ set03/problem01.c
@@ -20,3 +20,8 @@ float find_distance(float x1, float y1, float x2, float y2)
     float dist=sqrt(((x2-x1)*(x2-x1))-((y2-y1)*(y2-y1)));
     return dist;
 }
+
+void output(float x1, float y1, float x2, float y2, float distance)
+{
+    printf("")
+}
