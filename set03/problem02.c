43aa7c526e2b0a217f8e5b7996c19a1c0dc0c22d
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-10-31 12:00:22 +0530

    committed 10/31/2023, 12:00:21 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -9,7 +9,8 @@ int main()
 {
     float x1,x2,x3,y1,y2,y3,r;
     input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
-    r=
+    r=is_triangle(x1,y1,x2,y2,x3,y3);
+    output(x1,x)
 }
 
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3)
