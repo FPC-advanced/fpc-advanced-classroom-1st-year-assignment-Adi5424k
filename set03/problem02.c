91b8f64a35495cf8c17786ebb75a2dcf9ed3c675
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-10-31 12:01:52 +0530

    committed 10/31/2023, 12:01:51 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -43,7 +43,7 @@ void output(float x1, float y1, float x2, float y2,float x3, float y3, int resul
 {
     if(result==0)
     {
-        printf("The point (%f,%f), (%f,%f) and (%f,%f) form a Triangle",x1,y1,x2,y2,x3,y3);
+        printf("The point (%f,%f), (%f,%f) and (%f,%f) forms a Triangle",x1,y1,x2,y2,x3,y3);
     }
     else
     {
