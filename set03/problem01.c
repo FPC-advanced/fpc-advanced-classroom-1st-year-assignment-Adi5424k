aee72f7b8a0c96a1a9efee2906c510f935bb8758
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-10-31 11:14:44 +0530

    committed 10/31/2023, 11:14:43 AM

--- set03/problem01.c
+++ set03/problem01.c
@@ -7,6 +7,7 @@ void output(float x1, float y1, float x2, float y2, float distance);
 
 int main()
 {
+    
 }
 
 void input(float *x1, float *y1, float *x2, float *y2)
@@ -23,5 +24,5 @@ float find_distance(float x1, float y1, float x2, float y2)
 
 void output(float x1, float y1, float x2, float y2, float distance)
 {
-    printf("The distance between point (%f,%f) and (%f,%f) is %f",x2,y2,x1,y1,distance)
+    printf("The distance between point (%f,%f) and (%f,%f) is %f",x2,y2,x1,y1,distance);
 }
