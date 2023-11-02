14de2611edd9342ebf1665e69868d4a8aca5a543
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-11-02 16:06:11 +0530

    committed 11/2/2023, 4:06:10 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -43,10 +43,10 @@ void output(float x1, float y1, float x2, float y2,float x3, float y3, int resul
 {
     if(result==1)
     {
-        printf("The point (%f,%f), (%f,%f) and (%f,%f) forms a Triangle",x1,y1,x2,y2,x3,y3);
+        printf("The point (%f,%f), (%f,%f) and (%f,%f) forms a Triangle\n",x1,y1,x2,y2,x3,y3);
     }
     else
     {
-        printf("The point (%f,%f), (%f,%f) and (%f,%f) does not form a Triangle",x1,y1,x2,y2,x3,y3);
+        printf("The point (%f,%f), (%f,%f) and (%f,%f) does not form a Triangle\n",x1,y1,x2,y2,x3,y3);
     }
 }
---
+++ set03/problem05.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int input_array_size();
+void init_array(int n, int a[n]);
+void erotosthenes_sieve(int n, int a[n]);
+void output(int n, int a[n]);
+
+int main()
+{
+}
+
+int input_array_size()
+{
+    int x;
+    scanf("%d",&x);
+    return x;
+}
+
+void init_array(int n, int a[n])
+{
+    for(int i=0;i<n;i++)
+    {
+        a[i]=(i+1);
+    }
+}
+
+void erotosthenes_sieve(int n, int a[n])
+{
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]!=2 && a[i]!=3 && a[i]!=5)
+        {
+            if(a[i]%2==0 || a[i]%3==0 && a[i])
+        }
+    }
+}
