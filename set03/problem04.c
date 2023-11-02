1633d2b1e4638486bea136e239a640ffdb83fdff
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-11-02 10:10:23 +0530

    committed 11/2/2023, 10:10:22 AM

--- set03/problem04.c
+++ set03/problem04.c
@@ -17,11 +17,14 @@ int input()
 
 int find_fibo(int n)
 {
-    if(n==0)
+    if(n==1)
     {
         return 0;
     }
     else if(n==1 || n==2)
+    {
+        return 1;
+    }
     int a=0,b=1;
     for(int i=3;i<=n;i++)
     {
