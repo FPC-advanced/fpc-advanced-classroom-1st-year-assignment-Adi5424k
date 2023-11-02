9099946f3b04fab3e018a232daf3f6ba82a17bcc
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-11-02 10:05:53 +0530

    committed 11/2/2023, 10:05:51 AM

---
+++ set03/problem04.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int input();
+int find_fibo(int n);
+void output(int n, int fibo);
+
+int main()
+{
+}
+
+int input()
+{
+    int x;
+    scanf("%d",&x);
+    return x;
+}
+
+int find_fibo(int n)
+{
+    int a,b=0,1;
+    for(int i;i<=n;i++)
+    {
+
+    }
+}
