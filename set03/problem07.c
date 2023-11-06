2fb6737385a668e0523859b28a89fc378392b4f1
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-11-06 09:57:25 +0530

    committed 11/6/2023, 9:57:24 AM

--- set03/problem07.c
+++ set03/problem07.c
@@ -37,5 +37,7 @@ Line input_line()
 void find_length(Line *l)
 {
     float x=((l->p2.x-l->p1.x)*(l->p2.x-l->p1.x)+(l->p2.y-l->p1.y)*(l->p2.y-l->p1.y));
-    l->distance=sqrt(x);
+    l->distance=(x);
 }
+
+void output(Line l)
