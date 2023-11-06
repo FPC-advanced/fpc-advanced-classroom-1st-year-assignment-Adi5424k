87589325c6a34681fe7a6aa283bbb39bccfd5da4
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-11-06 09:45:25 +0530

    committed 11/6/2023, 9:45:24 AM

---
+++ set03/problem07.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+typedef struct point {
+    float x, y;
+} Point;
+
+typedef struct line {
+    Point p1, p2;
+    float distance;
+} Line;
+
+Point input_point();
+Line input_line();
+void find_length(Line *l);
+void output(Line l);
+
+int main()
+{
+}
+
+Point input_point()
+{
+    Point p;
+    scanf("%f%f",&p.x,&p.y);
+}
+
+Line input_line()
+{
+    Line l;
+    
+}
