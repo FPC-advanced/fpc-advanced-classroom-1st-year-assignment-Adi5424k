40b24c2f87db8cd92ea5e73f0506a70913de13de
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-11-07 11:22:45 +0530

    committed 11/7/2023, 11:22:44 AM

--- set04/problem01.c
+++ set04/problem01.c
@@ -26,6 +26,9 @@ void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den)
     else if(den2>den1)
     {
         int i=0;
-        for(i=0;i<den2)
+        for(i=0;i<den2;i++)
+        {
+            
+        }
     }
 }
