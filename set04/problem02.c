aa0f9f0090db79d115dec70d1c0b756ea4891b16
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-11-10 10:21:26 +0530

    committed 11/10/2023, 10:21:25 AM

--- set04/problem02.c
+++ set04/problem02.c
@@ -10,3 +10,35 @@ Fraction input()
     scanf("%d%d",&x.num,&x.den);
     return x;
 }
+
+Fraction smallest3(Fraction x, Fraction y, Fraction z)
+{
+    int xf,yf,zf;
+    xf=x.num/x.den;
+    yf=y.num/y.den;
+    zf=z.num/z.den;
+    int k=(xf<yf)?((xf<zf)?xf:zf):((yf<zf)?yf:zf);
+    if(k==xf)
+    {
+        return x;
+    }
+    else if(k==yf)
+    {
+        return y;
+    }
+    else
+    {
+        return z;
+    }
+}
+
+void output(Fraction x,Fraction y,Fraction z,Fraction r)
+{
+    printf("The smallest out of %d/%d, %d/%d and %d/%d is %d/%d.\n",x.num,x.den,y.num,y.den,z.num,z.den,r.num,r.den);
+}
+
+int main()
+{
+    Fraction x,y,z,r;
+    x=
+}
