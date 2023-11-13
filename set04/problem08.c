7d4e184a567468efa5141c5be9b033cf10b07d54
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-11-13 12:33:22 +0530

    committed 11/13/2023, 12:33:21 PM

--- set04/problem08.c
+++ set04/problem08.c
@@ -69,7 +69,7 @@ Fraction add_fractions(Fraction f1, Fraction f2)
 
 Fraction add_n_fractions(int n, Fraction f[n])
 {
-    Fraction sum=f[0];
+    Fraction sum={0,0};
     for(int i=0;i<n;i++)
     {
         
