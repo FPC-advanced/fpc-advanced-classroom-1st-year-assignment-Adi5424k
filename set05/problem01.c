8f1374e5bb6d2071dff3a517f1c50b6b72912eca
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-11-14 18:42:06 +0530

    committed 11/14/2023, 6:42:05 PM

---
+++ set05/problem01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct _point {
+  float x;
+  float y;
+};
+
+typedef struct _point Point;
+
+Point input();
+void dist(Point a, Point b, float *res);
+void output(Point a, Point b, float res);
+
+int main()
+{}
+
