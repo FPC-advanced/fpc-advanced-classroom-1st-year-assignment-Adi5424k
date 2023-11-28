f699c8de324ef41f98507c8715b65dd853d9069f
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-11-28 09:27:41 +0530

    committed 11/28/2023, 9:27:41 AM

--- set05/problem05.c
+++ set05/problem05.c
@@ -5,33 +5,36 @@ float borga_X(int x);
 void output(int x, float result);
 
 int main()
-{}
+{
+    int x;
+    x=input();
+    float borg; 
+    borg=borga_X(x);
+    output(x,borg);
+    return 0;
+}
 
 int input()
 {
     int x;
+    printf("Enter number: ");
     scanf("%d",&x);
     return x;
 }
 
-float factorial(int x)
-{
-    float result=1;
-    for(int i=0;i<x;i++)
-    {
-        result*=(float)(x-i);
-    }
-    return result;
-}
-
 float borga_X(int x)
 {
-    float borg=1,err=1,j=3;
-    int a=1;
+    float borg=1.0,err=1.0,j=1.0;
     while(err>0.000001)
     {
-        borg=(float)x/factorial(j);
-        err+=borg;
-        j=j+2;
+        err*=(float)(x)/((2*j)*(2*j+1));
+        borg+=err;
+        j++;
     }
+    return borg;
+}
+
+void output(int x, float result)
+{
+    printf("borga(%d)=%f\n",x,result);
 }
