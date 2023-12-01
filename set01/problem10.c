2172d54c842f2d995dacbacfed596969d44d942f
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-12-01 19:44:21 +0530

    committed 12/1/2023, 7:44:21 PM

--- set01/problem10.c
+++ set01/problem10.c
@@ -26,27 +26,16 @@ int stringcompare(char *string1, char *string2)
 {
     int i;
     for(i=0;string1[i]==string2[i] && string1[i]!='\0' && string2[i]!='\0';i++);
-    if(string1[i]>string2[i])
-    {
-        return 1;
-    }
-    else if(string1[i]<string2[i])
-    {
-        return 0;
-    }
-    else
-    {
-        return -1;
-    }
+    return (string1[i]-string2[i]);
 }
 
 void output(char *string1, char *string2, int result)
 {
-    if(result==1)
+    if(result>0)
     {
         printf("%s is greater than %s",string1,string2);
     }
-    else if(result==0)
+    else if(result<0)
     {
         printf("%s is greater than %s",string2,string1);
     }
