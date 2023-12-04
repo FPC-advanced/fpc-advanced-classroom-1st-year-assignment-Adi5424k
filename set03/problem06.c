08a3076b7e9fd0f9fba6fb6bff043a9977c6905d
Adi5424k <adityas.bsc23@rvu.edu.in>
2023-12-04 20:50:26 +0530

    committed 12/4/2023, 8:50:26 PM

--- set03/problem06.c
+++ set03/problem06.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 void input_string(char* a, char* b);
 int sub_str_index(char* string, char* substring);
@@ -24,27 +25,27 @@ void input_string(char* a, char* b)
 
 int sub_str_index(char* string, char* substring)
 {
-    int i=0,j=0,len1=0,len2=0;
-    for(len1=0;string[len1]!='\0';len1++);
-    for(len2=0;substring[len2]!='\0';len2++);
-    for(i=0;i<len1;i++)
+    int i=0,j=0;
+    int len1=strlen(string);
+    int len2=strlen(substring);
+    for(i=0;i<len1-len2+1;i++)
     {
-        if(string[i]==substring[0])
+        if(strncmp(string+i,substring,len2)==0)
         {
-            for(j=0;j<len2;j++)
-            {
-                if(substring[j]!=string[i])
-                {
-                    break;
-                }
-                else
-                { return i; }
-            }
+            return i;
         }
     }
+    return -1;
 }
 
 void output(char *string, char *substring, int index)
 {
-    printf("The index of '%s' in '%s' is %d\n",substring,string,index);
+    if(index==-1)
+    {
+        printf("The substring dosen't exist in the string.\n");
+    }
+    else
+    {
+        printf("The index of '%s' in '%s' is %d\n",substring,string,index);
+    }
 }
